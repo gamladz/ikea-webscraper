d2a3c510adf8f47568145526ace782126b248675
Gameli Ladzekpo <gameli.ladzekpo@gmail.com>
2021-02-17 18:00:54 +0000

    added firebase initialisation

--- bot.py
+++ bot.py
@@ -16,47 +16,59 @@ import boto3
 from botocore.exceptions import ClientError
 from selenium import webdriver 
 import time
+import requests
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Set up AWS functions
 s3 = boto3.resource("s3").Bucket("ikea-dataset")
 json.load_s3 = lambda f: json.load(s3.Object(key=f).get()["Body"])
 json.dump_s3 = lambda obj, f: s3.Object(key=f).put(Body=json.dumps(obj))
 client = boto3.client('s3', region_name='us-east-2')
 
-# json.dump_s3(data, "key")  saves json to s3://bucket/key
-# data = json.load_s3("key")  read json from s3://bucket/key
+# Example usage
+# Updated
+max_time = 10
 
-# open_chrome function
 def open_chrome(port=9220, on_mac=True):
     my_env = os.environ.copy()
     if on_mac:
+        print('opening chrome')
         subprocess.Popen(['open', '-a', "Google Chrome", '--args', f'--remote-debugging-port={port}', 'http://www.example.com'], env=my_env)
     else:
-        subprocess.Popen(f'google-chrome --remote-debugging-port={port} --user-data-dir=bots'.split(), env=my_env)
+        subprocess.Popen(f'google-chrome --remote-debugging-port={port} --user-data-dir=data_dir'.split(), env=my_env)
+    print('opened chrome')
 
 class Bot():
-    def __init__(self, port_no = 9220, headless = False, verbose = False):
+    def __init__(self, port_no=9220, headless=True, verbose=False):
         print('initialising bot')
 
-        open_chrome()
-
+        print(headless)
         options = Options()
-        options.add_argument("--no-sandbox")	# without this, the chrome webdriver can't start (SECURITY RISK)
-        options.add_experimental_option(f"debuggerAddress", f"127.0.0.1:{port_no}")	# attach to the same port that you're running chrome on
         if headless:
-            options.add_argument("--headless") # headless option allows scraper to run in the background
+            options.add_argument("--headless")
+            print('running headless')
+        else:
+            open_chrome(port=port_no)
+            options.add_experimental_option(f"debuggerAddress", f"127.0.0.1:{port_no}")	# attach to the same port that you're running chrome on
+        options.add_argument("--no-sandbox")	# without this, the chrome webdriver can't start (SECURITY RISK)
         #options.add_argument("--window-size=1920x1080")
-        self.driver = webdriver.Chrome('chrome_driver/chromedriver')			# create webdriver
+        self.driver = webdriver.Chrome('chrome_driver/chromedriver', chrome_options=options)	
         self.verbose = verbose
+    
+    def scroll(self, x=0, y=10000):
+        self.driver.execute_script(f'window.scrollBy({x}, {y})')
 
     def click_btn(self, text):
         if self.verbose: print(f'clicking {text} btn')
         element_types = ['button', 'div', 'input', 'a', 'label']
-        
         for element_type in element_types:
             btns = self.driver.find_elements_by_xpath(f'//{element_type}')
             # for btn in btns:
-            #   print(btn.text)
-
+            #     print(btn.text)
+            
             # SEARCH BY TEXT
             try:
                 btn = [b for b in btns if b.text.lower() == text.lower()][0]
@@ -88,23 +100,35 @@ class Bot():
     def toggle_verbose(self):
         self.verbose = not self.verbose
 
+    def download_file(self, src_url, local_destination):
+        response = requests.get(src_url)
+        with open(local_destination, 'wb+') as f:
+            f.write(response.content)
+
 if __name__ == '__main__':
-    # EXAMPLE USAGE
-    bot = Bot()
+    port = random.randint(9000,10000)
+    bot = Bot(port_no=port, headless=False)
+    
+    data_dict = {
+        
+    }
 
-    # data_dict = {"plant pots": [], "plates": [], "bin": []}
-    data_dict = {}
-
-    searches = {'tableware': [], 'plant pots':[], 'bin':[], 'cookware':[], 'wardrobes':[], 'tables':[], 'cabinets':[], 'clothing racks':[], 'chair':[]}
+    searches = {
+        "plant pots":[], 
+        "bin":[],
+        "cookware":[],
+        "wardrobes":[], 
+        "tables":[],
+        "cabinets":[],
+        "clothing racks":[],
+        "chair":[]
+    }
     
     for search in searches:
         bot.driver.get(f'https://www.ikea.com/gb/en/search/products/?q={search}')
         data_dict[search] = {}
 
-        # add a dict to a dict
-
-        # How to scroll on selenium
-
+        # Get full grid by trying to scroll and press show more button
         while True:
             try:
                 bot.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -124,37 +148,50 @@ if __name__ == '__main__':
 
         for i, result in enumerate(results):
 
-            print(result)
-            print(i)
-
+            # Skip promotional links, adverts or non-product links in grid
             if '/p/' not in result:
                 continue 
 
             bot.driver.get(result)
+            url = result
             result = result.split('/')[-2]
 
-            results_dict = {"id": [],"prod_name": [], "prod_price": [], "prod_desc": [], "prod_dimensions": [], "packaging": [], "prod_details": [], "sustainability": [], "images":[], "materials": [], "sustainability":[]}
+            # Set up dict for each products result
+            results_dict = {
+                "id": [],
+                "url":[],
+                "source":[],
+                "name": [],
+                "price": [],
+                "description": [],
+                "dims_image": [],
+                "dimensions": [],
+                "packaging": [],
+                "details": [],
+                "sustainability": [],
+                "images":[],
+                "materials":[]
+            }
             
-
-            results_dict['id'] = i
-
-
-            # Extract Product Description
+            results_dict['url'] = url
+            results_dict['id'] = hash(url)
+            results_dict['source'] = 'IKEA'
 
 
+            # Extract Product Name
             prod_name = bot.driver.find_element_by_xpath('//*[@class="range-revamp-header-section__title--big"]')
             prod_name = prod_name.get_attribute('innerHTML')              
-            results_dict['prod_name'] = prod_name
+            results_dict['name'] = prod_name
                         
-
+            # Product price
             prod_price = bot.driver.find_element_by_xpath('//*[@class="range-revamp-price__integer"]')
             prod_price = prod_price.get_attribute('innerHTML')
-            results_dict['prod_price'] = prod_price
+            results_dict['price'] = prod_price
 
 
             prod_desc = bot.driver.find_element_by_xpath('//*[@class="range-revamp-header-section__description-text"]')
             prod_desc = prod_desc.get_attribute('innerHTML')
-            results_dict['prod_desc'] = prod_desc
+            results_dict['description'] = prod_desc
 
 
             os.makedirs(f'data/{search}/{result}', exist_ok=True)
@@ -164,16 +201,35 @@ if __name__ == '__main__':
             prod_dims_images = [i.get_attribute('src') for i in prod_dims_images]
 
             for image in prod_dims_images:
-                
+                # Loop through each image and save to disk
 
                 for idx, img_url in enumerate(prod_dims_images):
+
+                    # Create filename for each with ID and get the file extension
                     filename = f'dims-{result}-{idx}'
                     file_ext = img_url.split('.')[-1] 
                     file_ext = file_ext[ 0 : 3 ]
-                    # urllib.request.urlretrieve(img_url, f'data/{search}/{result}/{filename}.{file_ext}')
-                    results_dict['dims_images'] = f'data/{search}/{result}/{filename}.{file_ext}'
+                    # Write file locally
+                    filepath = f'data/{search}/{result}/{filename}.{file_ext}'
+                    try:
+                        urllib.request.urlretrieve(img_url, filepath)
+                    except ConnectionResetError:
+                        print ('Connection Error')
+                    else:
+                        img_data = requests.get(img_url).content
+                        with open(filepath, 'wb') as handler:
+                            handler.write(img_data)                         
+                    finally:
+                        pass
+                    # Write image to AWS
+                    with open(filepath, 'rb') as data:
+                        s3.upload_fileobj(data, filepath)
+                    results_dict['dims_image'].append(filepath)
+                    # remove comment to stop storing locally
+                    # os.remove(filepath)
 
-
+            # for image in prod_dims_images:
+                
             
             # Get Product Details Packaging
             prod_packaging = bot.driver.find_elements_by_xpath('//*[@id="SEC_product-details-packaging"]//span')
@@ -181,7 +237,7 @@ if __name__ == '__main__':
             prod_packaging = [x for x in prod_packaging if "</span>" not in x]
             results_dict['packaging'] = prod_packaging
 
-            # Second get the description text
+            # Second get all product dimensions
             prod_dims_name = bot.driver.find_elements_by_xpath('//*[@class="range-revamp-product-dimensions__list-container"]//dt')
             prod_dims_name = [item.get_attribute('innerHTML').split(":")[0] for item in prod_dims_name]
         
@@ -189,28 +245,26 @@ if __name__ == '__main__':
             prod_dims_measure = bot.driver.find_elements_by_xpath('//*[@class="range-revamp-product-dimensions__list-container"]//dd')
             prod_dims_measure = [item.get_attribute('innerHTML') for item in prod_dims_measure]
             dims = dict(zip(prod_dims_name, prod_dims_measure))
-            results_dict['prod_dimensions'] = dims
+            results_dict['dimensions'] = dims
 
 
-            # Cycle through information on each page starting with images   
+            # Cycle through images 
             images = bot.driver.find_elements_by_xpath('//*[@class="range-revamp-media-grid__media-container"]//img') 
             images = [i.get_attribute('src') for i in images]
 
             # Product Details
             product_details = bot.driver.find_elements_by_xpath('//*[@class="range-revamp-product-details__paragraph"]')
-            
-
-            # Materials
             product_details = [paragraph.get_attribute('innerHTML') for paragraph in product_details]
             product_details = " ".join(product_details)
-            results_dict['prod_details'] = product_details
+            results_dict['details'] = product_details
 
+            # Materials
             product_details_materials = bot.driver.find_elements_by_xpath('//*[@id="SEC_product-details-material-and-care"]//span')
             product_details_materials = [paragraph.get_attribute('innerHTML') for paragraph in product_details_materials]
             product_details_materials = ",".join(product_details_materials)
             results_dict['materials'] = product_details_materials
 
-
+            # Sustainability
             product_details_sustain = bot.driver.find_elements_by_xpath('//*[@id="SEC_product-details-sustainability-and-environment"]//span')
             product_details_sustain = [paragraph.get_attribute('innerHTML') for paragraph in product_details_sustain]
             product_details_sustain = ",".join(product_details_sustain)
@@ -231,28 +285,29 @@ if __name__ == '__main__':
                     file_ext = file_ext[ 0 : 3 ]
                     # Write file locally
                     filepath = f'data/{search}/{result}/{filename}.{file_ext}'
-                    urllib.request.urlretrieve(img_url, filepath)
+                    try:
+                        urllib.request.urlretrieve(img_url, filepath)
+                    except ConnectionResetError:
+                        print ('Connection Error')
+                    else:
+                        img_data = requests.get(img_url).content
+                        with open(filepath, 'wb') as handler:
+                            handler.write(img_data)                         
+                    finally:
+                        pass
                     # Write to cloud
                     with open(filepath, 'rb') as data:
                         s3.upload_fileobj(data, filepath)
                     results_dict['images'].append(filepath)
-                    # remove file locally
-                    os.remove(filepath)
+                    # remove comment to remove file locally
+                    # os.remove(filepath)
                     
             # Remove duplicate images
             results_dict['images'] = [i for j, i in enumerate(results_dict['images']) if i not in results_dict['images'][:j]] 
-
-            data_dict[search][i] = results_dict
-
-
-            print(results_dict['images'])
-
-    
-            with open('data_json.txt', 'w') as json_file:
+            # Wrte dict to local
+            data_dict[search][i] = results_dict 
+            with open('data.json', 'w') as json_file:
                 json.dump(data_dict, json_file)
-            
+
+            # Write to AWS 
             json.dump_s3(data_dict, "data_json")
-
-
-
-        
